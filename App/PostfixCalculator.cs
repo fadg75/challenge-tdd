319947c75e1bedbb7ca19f5fb1e952bca0ddc33e
Faddey Kryachko <faddeyca@gmail.com>
2021-11-10 07:45:44 -0800

    Postfix Calculator

--- App/PostfixCalculator.cs
+++ App/PostfixCalculator.cs
@@ -8,7 +8,55 @@ namespace App
     {
         public static string Calculate(string postfixExpression)
         {
-            throw new NotImplementedException();
+            if (postfixExpression == null)
+            {
+                throw new FormatException();
+            }
+            if (postfixExpression.Length == 0)
+            {
+                return "0";
+            }
+            var array = postfixExpression.Split(' ');
+            int index = 0;
+            var st = new Stack<int>();
+            while(index < array.Length)
+            {
+                try
+                {
+                    if (array[index] == "+")
+                    {
+                        var x = st.Pop();
+                        var y = st.Pop();
+                        st.Push(x + y);
+                    }
+                    if (array[index] == "-")
+                    {
+                        var x = st.Pop();
+                        var y = st.Pop();
+                        st.Push(y - x);
+                    }
+                    if (array[index] == "*")
+                    {
+                        var x = st.Pop();
+                        var y = st.Pop();
+                        st.Push(x * y);
+                    }
+                    if (array[index] != "+" && array[index] != "-" && array[index] != "*")
+                    {
+                        st.Push(int.Parse(array[index]));
+                    }
+                }
+                catch
+                {
+                    throw new FormatException();
+                }
+                index++;
+            }
+            if(st.Count > 1)
+            {
+                throw new FormatException();
+            }
+            return st.Pop().ToString();
         }
     }
 }
